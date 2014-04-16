b45db396ba934a584a491cc8cd7a975805240cb8
Ben Radler <ben@benradler.com>
2014-04-15 22:52:23 -0700

    Use correct heroku env vars

--- config.js
+++ config.js
@@ -53,8 +53,8 @@ config = {
         database: {
             client: 'postgres',
             connection: {
-                host: process.env.DATABASE_URL,
-                user: process.env.DATABASE_USER,
+                host:     process.env.DATABASE_HOST,
+                user:     process.env.DATABASE_USER,
                 password: process.env.DATABASE_PASSWORD,
                 database: process.env.DATABASE_NAME,
                 port: '5432'
